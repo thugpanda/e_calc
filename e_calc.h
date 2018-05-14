31e77e897d9a3527e1f63a03d20b7ec6b2d93671
thugpanda <webmaster@thugpanda.de>
2018-05-14 16:32:57 +0200

    Added better explanation for Schmitt Trigger values

---
+++ e_calc.h
@@ -0,0 +1,14 @@
+#ifndef E_CALC
+#define E_CALC
+
+float   calcCurrent();
+float   calcCurrentDensity();
+float   calcImpedance();
+float   calcParallelResistance();
+float   calcPower();
+void    printResults(int type);
+float   calcResistance();
+float   calcVoltage1();
+float   calcVoltage2();
+
+#endif
